ffed4254b1dc10ce9bc6f20c17187ed072b634dc
AlfinMHS <alfin_2327240071@mhs.mdp.ac.id>
2026-04-14 11:49:47 +0700

    comitpertama

---
+++ scripts.js
@@ -0,0 +1,35 @@
+let nama = document.getElementById("nama");
+let barang = document.getElementById("barang");
+let jumlah = document.getElementById("jumlah");
+
+function simpan() {
+    console.log(npm.value)
+    console.log(nama.value)
+    console.log(image.value)
+
+    // localStorage.setItem("npm", npm.value)
+    // localStorage.setItem("nama", nama.value)
+
+    //jika local storage belum ada isi/value
+    if (localStorage.getItem("mahasiswa") === null) {
+        //simpan array kosong[]
+        localStorage.setItem("mahasiswa", "[]")
+    }
+    //panggil local storage(konvers String ke object)
+    let data = JSON.parse(localStorage.getItem("mahasiswa"))
+    console.log(data)
+
+    //simpan value.npm dan nama ke dalam object
+    data.push({
+        npm: npm.value,
+        nama: nama.value,
+        image: image.value
+    })
+    console.log(data)
+
+    //simpan data terbaru kedalam local storage
+    //konversi dari objek ke string
+    localStorage.setItem("mahasiswa", JSON.stringify(data))
+    //panggil tampil
+    tampil()
+}
